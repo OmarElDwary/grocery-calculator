a70176c86c194bd576677ac1141ec1a637d1ec3d
OmarElDwary <omareldwary91@gmail.com>
2024-12-18 16:00:45 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Grocery Calculator App
+
+This App calculates the total cost of the grocery items and adds taxes to them
+
+# Features
+- Calculate the total cost of the grocery before taxes
+- Adds the constant variable of taxes which is 10%
+- Returns the total before and after taxes
+
+# Credientials
+**Omar Shaban Ahmed Abdelaziz**
+Github: **OmarElDwary**
---
+++ main.dart
@@ -0,0 +1,26 @@
+void main() {
+  // I have created a list of grocery items that contains a map for each item with its name and price
+  List<Map<String, dynamic>> groceryItems = [
+    {"name": "Bread", "price": 10},
+    {"name": "Eggs", "price": 7},
+    {"name": "Water", "price": 1.4},
+    {"name": "Meat", "price": 12.8},
+    {"name": "Soap", "price": 0.6},
+  ];
+
+  // constatnt tax rate of 10%
+  const double taxRate = 0.10;
+
+  // instatnce of total price variable
+  double totalPrice = 0;
+  // loop around the grocery items to calculate the total price
+  for (var item in groceryItems) {
+    totalPrice += item["price"];
+  }
+
+  // calculate after taxes
+  double totalAfterTax = totalPrice + (totalPrice * taxRate);
+
+  print(totalPrice);
+  print(totalAfterTax);
+}
